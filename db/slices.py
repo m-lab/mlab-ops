f67b0b0c81d85eda9b09dccc0475f267261ff676
Stephen Soltesz <stephen.soltesz@gmail.com>
2012-10-25 17:16:17 -0400

    add ipv6 addresses to all slices on mlab4.nuq02

--- db/slices.py
+++ db/slices.py
@@ -8,12 +8,12 @@ from planetlab.types import *
 # attr  : a list of slice attributes.
 slice_list = [
 
-    Slice(name='gt_partha',              index=0, ipv6=['mlab4.nuq01']),
+    Slice(name='gt_partha',              index=0, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
     Slice(name='iupui_ndt',              index=1, attrs=[
                     Attr('MeasurementLab',    capabilities='VXC_PROC_WRITE'),
                     Attr('MeasurementLab',    disk_max='60000000'),
                     Attr('MeasurementLabK32', disk_max='60000000'), ],
-                    ipv6=['mlab4.nuq01']),
+                    ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
 
     Slice(name='iupui_npad',             index=2, attrs=[
                     Attr(None,                initscript='iupui_npad_initscript'),
@@ -31,18 +31,18 @@ slice_list = [
                     Attr('MeasurementLab',    disk_max='35000000'),
                     Attr('MeasurementLabK32', capabilities='CAP_NET_RAW'),
                     Attr('MeasurementLabK32', disk_max='35000000'), ],
-                    ipv6=['mlab4.nuq01']),
+                    ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
 
-    Slice(name="northwestern_windrider", index=4, ipv6=['mlab4.nuq01']),
-    Slice(name="uw_geoloc4",             index=5, ipv6=['mlab4.nuq01']),
-    Slice(name="samknows_ispmon",        index=7, ipv6=['mlab4.nuq01']),
-    Slice(name="gt_bismark",             index=8, ipv6=['mlab4.nuq01']),
-    Slice(name="mlab_neubot",            index=9, ipv6=['mlab4.nuq01', 'mlab1.trn01']),
-    Slice(name="michigan_1",             index=10, ipv6=['mlab4.nuq01']),
+    Slice(name="northwestern_windrider", index=4, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
+    Slice(name="uw_geoloc4",             index=5, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
+    Slice(name="samknows_ispmon",        index=7, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
+    Slice(name="gt_bismark",             index=8, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
+    Slice(name="mlab_neubot",            index=9, ipv6=['mlab4.nuq01', 'mlab1.trn01', 'mlab4.nuq02']),
+    Slice(name="michigan_1",             index=10, ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
     Slice(name='princeton_namecast',     index=11, attrs=[
                     Attr('MeasurementLab',    capabilities='CAP_NET_BIND_SERVICE'),
                     Attr('MeasurementLabK32', capabilities='CAP_NET_BIND_SERVICE'), ],
-                    ipv6=['mlab4.nuq01']),
+                    ipv6=['mlab4.nuq01', 'mlab4.nuq02']),
 
     Slice(name="pl_netflow"),
     Slice(name="pl_default"),
