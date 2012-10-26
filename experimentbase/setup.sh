a938a806142a100f08b464025f2e43b2896aa8ba
soltesz@opentechinstitute.org <soltesz@opentechinstitute.org>
2012-10-26 18:31:11 -0400

    build files more better goodly

--- experimentbase/setup.sh
+++ experimentbase/setup.sh
@@ -6,10 +6,17 @@ if [ -z "$1" ] ; then
     echo "please provide slice name as only argument"
     exit 1
 fi
-mkdir build
+mkdir -p build
 pushd initscript
-    ./build.sh > ../build/bootstrap.sh
+    sed -e '/ALLFUNCTIONS/ { 
+      r bootstrap-functions
+      d 
+    }' init.sh > ../build/bootstrap.sh
 popd
 tar -C smp -zcvf build/slice-management-package.tar.gz .
-tar -C spp -zcvf build/$1.tar.gz .
+tar -C slice_example -zcvf build/$1.tar.gz .
+
+if [ -n "$2" ] && [ "$2" = "install" ] ; then
+    scp -r build/*.tar.gz ks:mlabops/keys/
+fi
 
