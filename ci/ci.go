5cf50b7a0740ab77e227765dd591281729a1d75c
Dominic Hamon <dominich@google.com>
2013-01-03 09:45:29 -0800

    Initial version of CI app for m-lab

---
+++ ci/ci.go
@@ -0,0 +1,23 @@
+package ci
+
+import (
+	//"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	//redirect_map := map[string]string {
+	//	"libraries": "https://drone.io/dominic-mlab/m-lab.libraries?key=BV8KN727SQ1JMEK7DKIGSO97SLBDJL2O",
+	//	"ns": "https://drone.io/dominic-mlab/m-lab.ns?key=S4MHVE51D5KN5SGK1IOV1TA0SGK21RBF",
+	//}
+
+//	if r.Method == "POST" {
+		r.Header.Write(w);
+		// TODO: post to drone.io based on contents
+		//http.Redirect(w, r, redirect_map["libraries"], http.StatusFound)
+//	}
+}
