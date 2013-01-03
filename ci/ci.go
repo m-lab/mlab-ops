7f446468888007476823772b34af0d08ec720a53
Dominic Hamon <dominic@measurementlab.net>
2013-01-03 10:22:20 -0800

    Log body of request to track down details

--- ci/ci.go
+++ ci/ci.go
@@ -2,6 +2,8 @@ package ci
 
 import (
 	"appengine"
+	"io/ioutil"
+	//"json"
 	"net/http"
 )
 
@@ -15,10 +17,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	//	"ns": "https://drone.io/dominic-mlab/m-lab.ns?key=S4MHVE51D5KN5SGK1IOV1TA0SGK21RBF",
 	//}
 
-//	if r.Method == "POST" {
+	if r.Method == "POST" {
 		c := appengine.NewContext(r)
 		c.Debugf("Request: %#v", r)
+
+		var b []byte;
+		b, err := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+
+		if err != nil {
+			c.Errorf("Failed to read body: %v", err)
+		} else {
+			c.Debugf("Body: %s", string(b))
+			//err := json.Unmarshal(b, &m)
+		}
 		// TODO: post to drone.io based on contents
 		//http.Redirect(w, r, redirect_map["libraries"], http.StatusFound)
-//	}
+	}
 }
