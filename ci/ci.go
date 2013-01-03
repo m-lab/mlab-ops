7e3aa82c1b4a06f0fc2fbf7a5794a8e174fe8f99
Dominic Hamon <dominic@measurementlab.net>
2013-01-03 10:46:53 -0800

    Attempt to read the repository path

--- ci/ci.go
+++ ci/ci.go
@@ -2,14 +2,16 @@ package ci
 
 import (
 	"appengine"
+	"encoding/json"
 	"io/ioutil"
-	//"json"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
+
+var expected_user_agent string = "Google Code Project Hosting (+http://code.google.com/p/support/wiki/PostCommitWebHooks)"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	//redirect_map := map[string]string {
@@ -21,16 +23,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		c := appengine.NewContext(r)
 		c.Debugf("Request: %#v", r)
 
+		if r.Header["User-Agent"][0] != expected_user_agent {
+			c.Errorf("Unexpected user agent: %v", r.Header["User-Agent"])
+			http.Error(w, "Unexpected user agent", http.StatusForbidden)
+			return
+		}
+
 		var b []byte;
 		b, err := ioutil.ReadAll(r.Body)
 		r.Body.Close()
 
 		if err != nil {
 			c.Errorf("Failed to read body: %v", err)
-		} else {
-			c.Debugf("Body: %s", string(b))
-			//err := json.Unmarshal(b, &m)
+			return
 		}
+		c.Debugf("Body: %s", string(b))
+		var f interface{}
+		err = json.Unmarshal(b, &f)
+		if err != nil {
+			c.Errorf("Failed to unmarshal body: %s", string(b))
+			return
+		}
+		m := f.(map[string]interface{})
+		c.Debugf("Repository: %s", m["repository_path"])
 		// TODO: post to drone.io based on contents
 		//http.Redirect(w, r, redirect_map["libraries"], http.StatusFound)
 	}
