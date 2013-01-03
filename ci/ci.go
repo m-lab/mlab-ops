7ad7869cd77fc6536eefe835f3ecb0c8a394c187
Dominic Hamon <dominic@measurementlab.net>
2013-01-03 11:07:34 -0800

    Extract the path and post to drone.io

--- ci/ci.go
+++ ci/ci.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -12,14 +13,15 @@ func init() {
 }
 
 var expected_user_agent string = "Google Code Project Hosting (+http://code.google.com/p/support/wiki/PostCommitWebHooks)"
+var repository_path_prefix string = "https://code.google.com/p/m-lab."
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	//redirect_map := map[string]string {
-	//	"libraries": "https://drone.io/dominic-mlab/m-lab.libraries?key=BV8KN727SQ1JMEK7DKIGSO97SLBDJL2O",
-	//	"ns": "https://drone.io/dominic-mlab/m-lab.ns?key=S4MHVE51D5KN5SGK1IOV1TA0SGK21RBF",
-	//}
-
 	if r.Method == "POST" {
+		redirect_map := map[string]string {
+			"libraries": "https://drone.io/dominic-mlab/m-lab.libraries?key=BV8KN727SQ1JMEK7DKIGSO97SLBDJL2O",
+			"ns": "https://drone.io/dominic-mlab/m-lab.ns?key=S4MHVE51D5KN5SGK1IOV1TA0SGK21RBF",
+		}
+
 		c := appengine.NewContext(r)
 		c.Debugf("Request: %#v", r)
 
@@ -45,8 +47,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		m := f.(map[string]interface{})
-		c.Debugf("Repository: %s", m["repository_path"])
-		// TODO: post to drone.io based on contents
-		//http.Redirect(w, r, redirect_map["libraries"], http.StatusFound)
+		repository_path := m["repository_path"].(string)
+		c.Debugf("Repository: %s", repository_path)
+
+		if !strings.HasPrefix(repository_path, repository_path_prefix) {
+			c.Errorf("Unexpected repository path: %s", repository_path)
+			return
+		}
+		repo := repository_path[len(repository_path_prefix):-1]
+		var url string
+		var exists bool
+		if url, exists = redirect_map[repo]; !exists {
+			c.Warningf("No redirect for repo %s", repo)
+			return
+		}
+
+		// post to drone.io based on contents
+		c.Infof("Forwarding request for repo %s to %s", repo, url)
+		http.Redirect(w, r, url, http.StatusFound)
 	}
 }
