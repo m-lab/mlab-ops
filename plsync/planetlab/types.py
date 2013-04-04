b8c472e0c51161cc904a74bf7ac6121d558f7e7b
soltesz@opentechinstitute.org <soltesz@opentechinstitute.org>
2013-04-04 13:10:34 -0400

    Adds Location() support, and many doc strings

--- plsync/planetlab/types.py
+++ plsync/planetlab/types.py
@@ -52,7 +52,28 @@ def pl_dracip(host_index, v4prefix):
 def pl_v6gw(v6prefix, v6gw=None):
     return v6prefix + "1" if v6gw is None else v6gw
 
+class Location(dict):
+    def __init__(self, city, country, lat, lon, **kwargs):
+        self['city'] = city
+        self['country'] = country
+        self['latitude'] = lat
+        self['longitude'] = lon
+        super(Location, self).__init__(**kwargs)
+
 class Network(dict):
+    """The Network() object encapsulates the IP and network objects for the IPv4
+    and IPv6 settings for an M-Lab site.  
+
+    Network() constructor expects two parameters:
+        v4 - string, a /26 IPv4 prefix i.e. 192.168.10.0
+        v6 - string, a /64 IPv6 prefix i.e. 2604:ca00:f000::
+        v6gw - optional, string, a specific gateway other than the 
+               default, <prefix>::1
+
+    Attributes:
+        Network['v4'] is a NetworkIPv4() object
+        Network['v6'] is a NetworkIPv6() object
+    """
     legacy_network_remap = None
     def __str__(self):
         return pprint.pformat(self)
@@ -75,6 +96,17 @@ class Network(dict):
         super(Network, self).__init__(**kwargs)
 
 class NetworkIPv6(dict):
+    """The NetworkIPv6() object encapsulates operations for IPv6 network
+    configuration on M-Lab sites and nodes.  It has built-in methods for
+    extracting per-node attributes and managing IP assignment to slices.
+
+    NetworkIPv6() constructor expects these parameters:
+        prefix - string, a /64 IPv6 prefix i.e. 2604:ca00:f000::
+        last_octet - string the last octet of the IPv4 site prefix. This
+                    value is used to offset all addresses.  Should be one of 0,
+                    64, 128, or 192.
+        v6gw - a specific gateway, if None, defaults to <prefix>::1
+    """
     def __str__(self):
         return pprint.pformat(self)
     def __init__(self, **kwargs):
@@ -91,12 +123,17 @@ class NetworkIPv6(dict):
         super(NetworkIPv6, self).__init__(**kwargs)
 
     def ipv6_defaultgw(self):
+        """ Returns the IPv6 gateway as calculated from prefix & v6gw """
         return pl_v6gw(self['prefix'], self['v6gw'])
 
-    def ipv6addr(self, index):
-        return pl_v6_primary(index, self['prefix'], int(self['last_octet']))
+    def ipv6addr(self, host_index):
+        """ Returns the host IPv6 address for the host_index node; host_index
+        should be less than 4, the maximum number of nodes at a site."""
+        return pl_v6_primary(host_index, self['prefix'], 
+                             int(self['last_octet']))
 
     def ipv6addr_secondaries(self, index):
+        """ Returns a list of 12 IPv6 addresses assigned to given host_index """
         # NOTE: the natural, sorted order is re-ordered according to 
         #       legacy_network_remap if present.
         ipv6_list = pl_v6_iplist(index, self['prefix'], int(self['last_octet']))
@@ -112,6 +149,13 @@ class NetworkIPv6(dict):
         return ipv6_list
 
 class NetworkIPv4(dict):
+    """The NetworkIPv4() object encapsulates the IP and network settings for an
+    M-Lab site.  It has built-in methods for extracting per-node attributes and
+    managing IP assignment to slices.
+
+    NetworkIPv4() constructor expects these parameters:
+        prefix - string, a /26 IPv4 prefix i.e. 192.168.10.0
+    """
     def __str__(self):
         return pprint.pformat(self)
     def __init__(self, **kwargs):
@@ -121,9 +165,11 @@ class NetworkIPv4(dict):
         super(NetworkIPv4, self).__init__(**kwargs)
 
     def interface(self, index):
+        """ Returns the myPLC interface definition for the given host index"""
         return pl_interface(index, self['prefix'])
 
     def iplist(self, index):
+        """ Returns a list of 12 IPv4 addresses for the given host index """
         ip_list = pl_iplist(index, self['prefix'])
         if (Network.legacy_network_remap is not None and 
             self['name'] in Network.legacy_network_remap and
@@ -136,15 +182,35 @@ class NetworkIPv4(dict):
         return ip_list 
 
     def drac(self, index):
+        """ Returns the IPv4 address reserved for the DRAC interface"""
         return pl_dracip(index, self['prefix'])
 
     def last(self):
+        """ Returns the last octet of 'prefix' """
         l = self['prefix'].split('.')[3]
         return int(l)
 
 class Site(dict):
+    """Site() - represents an M-Lab site.  Also wraps the creation of site 
+        Node()s and PCU()s.
+
+       Site() constructor expects:
+            name - a short name for a site, i.e. nuq01
+            net  - a Network() object consisting of at least an IPv4 prefix
+
+          Optional:
+            count - the number of nodes at the site (default: 3)
+            nodegroup - the nodegroup with which to associate new nodes at 
+                        this site.  (default: MeasurementLab)
+            pi - a list of people to add as PI's for a new site.
+                 (default: Stephen Stuart)
+            login_base_prefix - a constant prefix for to prepend to 'name' 
+                                (default: mlab).
+
+    """
     def __str__(self):
         return pprint.pformat(self)
+
     def __init__(self, **kwargs):
         if 'name' not in kwargs:
             raise Exception("'name' is a mandatory argument. i.e. nuq01, lga02")
@@ -159,10 +225,14 @@ class Site(dict):
             kwargs['pi'] = [ ("Stephen","Stuart","sstuart@google.com") ]
         if 'login_base_prefix' not in kwargs:
             kwargs['login_base_prefix'] = 'mlab'
+        if 'location' not in kwargs:
+            kwargs['location'] = None
 
         if kwargs['net'] is not None:
-            kwargs['net']['v4']['name'] = kwargs['name']
-            kwargs['net']['v6']['name'] = kwargs['name']
+            if kwargs['net']['v4'] is not None:
+                kwargs['net']['v4']['name'] = kwargs['name']
+            if kwargs['net']['v6'] is not None:
+                kwargs['net']['v6']['name'] = kwargs['name']
         else:
             # 'net' is None only if there are no nodes for this site
             assert(kwargs['count'] == 0)
@@ -190,8 +260,13 @@ class Site(dict):
 
         super(Site, self).__init__(**kwargs)
 
-    def sync(self, onhost=None, skipinterfaces=False):
+    def sync(self, onhost=None, skipinterfaces=False, getbootimages=False):
+        """ Do whatever is necessary to validate this site in the myplc DB.
+            Actions may include creating a new Site() entry in myPLC DB, 
+            creating people listed as PIs, creating nodes and PCUs.  
+        """
         MakeSite(self['login_base'], self['sitename'], self['sitename'])
+        SyncLocation(self['login_base'], self['location'])
         for person in self['pi']:
             p_id = MakePerson(*person)
             email = person[2]
@@ -199,15 +274,43 @@ class Site(dict):
             AddPersonToSite(email,p_id,"pi",self['login_base'])
         for hostname,node in self['nodes'].iteritems():
             if onhost is None or hostname == onhost:
-                node.sync(skipinterfaces)
+                node.sync(skipinterfaces, getbootimages)
+
+def makesite(name, v4prefix, v6prefix, city, country, 
+             latitude, longitude, pi_list, **kwargs):
+    v6gw=None               # use default
+    if 'v6gw' in kwargs:    # but, if provided
+        v6gw=kwargs['v6gw'] # save for Network() object below
+        del kwargs['v6gw']  # and don't pass to Site()
+    location=None
+    if city is not None:
+        # NOTE: only create Location() if city* is specified.
+        location = Location(city, country, latitude, longitude)
+    return Site(name=name, 
+                net=Network(v4=v4prefix, v6=v6prefix, v6gw=v6gw),
+                location=location,
+                pi=pi_list,
+                **kwargs)
 
 class PCU(dict):
+    """ PCU() - represents an M-Lab PCU at a parent Site()
+
+    PCU() constructor expects:
+        name - the site name, i.e. nuq01
+        net - the NetworkIPv4() object representing the parent site.
+        index - the host index for the machine this PCU is associated with.
+
+      Optional:
+        username - drac username (default: admin)
+        password - drac password (default: changeme)
+        model    - drac model (default: DRAC)
+    """
     def __str__(self):
         #return pprint.pformat(self)
         return self.fields()
     def __init__(self, **kwargs):
         if 'name' not in kwargs:
-            raise Exception("'name' is a mandatory argument. i.e. FQDN")
+            raise Exception("'name' is a mandatory argument. i.e. nuq01")
         if 'net' not in kwargs:
             raise Exception("'net' is a mandatory argument.")
         if 'index' not in kwargs:
@@ -222,9 +325,11 @@ class PCU(dict):
         super(PCU, self).__init__(**kwargs)
 
     def hostname(self):
+        """ generate the hostname for this DRAC based on index & site name """
         return "mlab%dd.%s.measurement-lab.org" % (self['index'], self['name'])
 
     def fields(self):
+        """ return a dict() with the PCU values for use by myplc AddPCU() """
         return { 'username': self['username'],
                  'password': self["password"],      # password is updated later.
                  'model'   : self['model'],
@@ -232,6 +337,14 @@ class PCU(dict):
                  'hostname': self.hostname() }
         
 class Node(dict):
+    """ Node() - represents an M-Lab node at a parent Site().
+    Node() constructor expects these parameters:
+        name - the site name, i.e. nuq01
+        index - the host index for the machine this PCU is associated with.
+        net - the Network() object representing the parent site, will contain
+                both ipv4 & ipv6 information (if present).
+        exclude_ipv6 - whether or not to exclude ipv6 from the configuration
+    """
     def __str__(self):
         return str({ 'interface' : self.interface(),
                      'iplist'    : self.iplist(),
@@ -266,6 +379,7 @@ class Node(dict):
 
     def hostname(self):
         return "mlab%d.%s.measurement-lab.org"  % (self['index'], self['name'])
+
     def v6interface_tags(self):
         secondary_list = self['net']['v6'].ipv6addr_secondaries(self['index'])
         goal = {
@@ -285,6 +399,7 @@ class Node(dict):
         return (self['net'] is not None and self['net']['v6'] is not None)
 
     def get_interface_attr(self, slice_obj):
+        """ Used to construct the Interface() object for this node in myplc """
         attr = None
         if slice_obj['index'] is None:
             return None
@@ -331,32 +446,53 @@ class Node(dict):
 
         return attr
 
-    def sync(self, skipinterfaces=False):
+    def sync(self, skipinterfaces=False, getbootimages=False):
+        """Create and or verify that Node object & PCU & interface is created in
+        myplc db"""
+
         node_id = MakeNode(self['login_base'], self.hostname())
         MakePCU(self['login_base'], node_id, self['pcu'].fields())
         PutNodeInNodegroup(self.hostname(), node_id, self['nodegroup'])
         interface = self.interface()
         if not skipinterfaces:
-            MakeInterface(self.hostname(), node_id, interface,
+            SyncInterface(self.hostname(), node_id, interface,
                           interface['is_primary'])
-            goal = { "ifname" : "eth0", "ovs_bridge" : "public0"}
-            MakeInterfaceTags(node_id, interface, goal)
+            if self['nodegroup'] == 'MeasurementLabLXC':
+                # NOTE: these tags are needed on the primary interface 
+                #       for the lxc build of PlanetLab
+                goal = { "ifname" : "eth0", "ovs_bridge": "public0"}
+                SyncInterfaceTags(node_id, interface, goal)
 
         if not self['exclude_ipv6']:
-            MakeInterfaceTags(node_id, interface, self.v6interface_tags())
+            SyncInterfaceTags(node_id, interface, self.v6interface_tags())
 
         if not skipinterfaces and self['nodegroup'] != 'MeasurementLabLXC':
             for ip in self.iplist():
                 interface['ip'] = ip
                 interface['is_primary'] = False
-                MakeInterface(self.hostname(), node_id, interface, 
+                SyncInterface(self.hostname(), node_id, interface, 
                               interface['is_primary'])
+        if getbootimages:
+            GetBootimage(self.hostname(), imagetype="iso")
+            
         return 
 
 class Attr(dict):
-    #def __str__(self):
-    #    #return pprint.pformat(self)
-    #    return str(self)
+    """Attr() are attributes of a slice, i.e. a key=value pair.
+    
+    Slice attributes apply key=value pairs to some context. Possible contexts
+    are 'all nodes', 'only a specific node', 'only a specific nodegroup'.
+
+    Attr() constructor expects one argument, and a key=value pair.
+        arg[0] - the context for this slice attribute; may be one of: 
+                 None - which represents all hosts
+                 <hostname> - a hostname recognized by having a '.'
+                 <nodegroup> - a nodegroup name, recognized by not having a '.'
+        key=value - the key and value are not arbitrary.  The key must be one 
+                 of a pre-defined set of recognized keys defined by the 
+                 PlanetLab api.  The value for a given key, should be a valid 
+                 value. Though, no type checking is performed here.
+    """
     def __init__(self, *args, **kwargs):
         if len(args) != 1:
             raise Exception(("The first argument should be the name "+
@@ -377,6 +513,21 @@ class Attr(dict):
         super(Attr, self).__init__(**kwargs)
 
 class Slice(dict):
+    """ Slice() - represents an M-Lab slice.  Provides an interface for passing
+        additional slice attributes, and associating IP addresses. 
+
+    Slice() constructor expects the following parameters:
+        name - the slice name, i.e. 'iupui_ndt', or 'mlab_neubot'
+      Optional:
+        index - int, the index in the 12-slots for slices with IPv4 addresses.
+        attrs - [], a list of Attr() objects with attributes for this slice.
+        ipv6 - how to enable IPv6 for this slice. Options are:
+                "all" - add IPv6 addres to all nodes
+                [] - a list of abbreviated hostnames, i..e ['mlab1.nuq01', 
+                    'mlab2.nuq02', ...]
+                None - do not enble IPv6 addressing anywhere.
+    """
+
     def __str__(self):
         return "\n%s \n\t %s" % (self['name'], pprint.pformat(self))
 
@@ -399,11 +550,8 @@ class Slice(dict):
                 raise Exception("Unrecognized type for ipv6 parameter: %s" % 
                                     type(kwargs['ipv6']))
 
-        # *ALL* K32 kernels will need the isolate_loopback enabled.
-        LOOPBACK=Attr('MeasurementLabK32', isolate_loopback='1')
         if 'attrs' not in kwargs:
             kwargs['attrs'] = []
-        kwargs['attrs'] += [LOOPBACK]
         kwargs['network_list'] = []
 
         super(Slice, self).__init__(**kwargs)
@@ -416,13 +564,16 @@ class Slice(dict):
                 (isinstance(self['ipv6'], str) and "all" == self['ipv6']) )
         
     def sync(self, hostname_or_site=None, skipwhitelist=False, 
-                                          skipsliceips=False):
-        # NOTE: SLICES ARE NOT CREATED HERE.
-        #       USERS  ARE NOT ADDED TO SLICES HERE.
+             skipsliceips=False, createslice=False):
+        """ Create and/or verify the object in the myplc DB """
+        # NOTE: USERS  ARE NOT ADDED TO SLICES HERE.
+        if createslice:
+            print "Making slice! %s" % self['name']
+            MakeSlice(self['name'])
         SyncSliceExpiration(self['name'])
 
         for attr in self['attrs']:
-            MakeSliceAttribute(self['name'], attr)
+            SyncSliceAttribute(self['name'], attr)
         for h,node in self['network_list']:
             if ( hostname_or_site is None or 
                  hostname_or_site == h    or 
@@ -430,10 +581,11 @@ class Slice(dict):
                 if not skipwhitelist:
                     # add this slice to whitelist of all hosts.
                     WhitelistSliceOnNode(self['name'], h)
+                    #RemoveSliceFromNode(self['name'], h)
                 if not skipsliceips:
                     attr = node.get_interface_attr(self)
                     if attr:
-                        MakeSliceAttribute(self['name'], attr)
+                        SyncSliceAttribute(self['name'], attr)
 
         return
 
